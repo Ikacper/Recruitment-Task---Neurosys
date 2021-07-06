6aeed17f115eae0b4ec2baa9a1a6c1fff1664a46
Kacper Ratyński <kacperratynski1@gmail.com>
2021-07-06 22:29:19 +0200

    added activate class in navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,19 +6,13 @@ function Navbar() {
   return (
     <ul className={styles.navLinks}>
       <NavLink exact to='/' activeClassName={styles.selected}> 
-        <div className={styles.link}>
            home
-        </div>
       </NavLink>
       <NavLink to='/about' activeClassName={styles.selected}>
-        <div className={styles.link}>
           about us 
-        </div>  
       </NavLink>  
       <NavLink to='/contact' activeClassName={styles.selected}>
-        <div className={styles.link}>
           contact 
-        </div>  
       </NavLink> 
     </ul>
   )
